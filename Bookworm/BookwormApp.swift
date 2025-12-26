c6f2d8e2e7e35b2ea4f8e7557ba012e65bd0f8ef
Hafizur Rahman <hafizur.rahman.cs@gmail.com>
2025-12-26 13:58:07 +0600

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Hafizur Rahman on 26/12/25.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
